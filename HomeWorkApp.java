972da2b721360c093e127d08c1a981e6c7d5b157
Catherine <bezmozggoff@gmail.com>
2022-02-28 14:09:45 +0700

    change Java 1. Lesson 2

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -41,14 +41,6 @@ public class HomeWorkApp {
     }
 
     private static boolean checkLeapYear(int year) { //високосный ли год
-        if (year % 400 == 0) {
-            return true;
-        }
-
-        if(year % 4 == 0 && year % 100 != 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
